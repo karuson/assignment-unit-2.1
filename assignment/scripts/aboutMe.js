ff60f2f4a4575664e9ef8a322d8f0e39ce6cfc8b
Ben Lynch <bdlynch7@gmail.com>
2023-06-16 09:02:12 -0500

    conditiionals

--- assignment/scripts/aboutMe.js
+++ assignment/scripts/aboutMe.js
@@ -24,7 +24,7 @@ console.log( 'the lucky number is ', luckyNumber );
 console.log( 'My name is', fullName, 'and I think', luckyNumber, 'is a winner!' )
 
 // 7 - Create a variable named `adventurous` and set it to a boolean value (true or false)
-let adventurous = true;
+let adventurous = false;
 
 // 8 - Create a variable named `food`, and set its value to a string of your favorite food
 let food = 'chicken strips';
@@ -48,16 +48,33 @@ console.log( 'the number of pets allowed is', allowedPets );
 
 // 13 - Create a conditional: if adventurous is true, console log "Adventures are great!", 
 // if it's not true, console log "How about we stay home?"
-
+if ( adventurous ) {
+    console.log( 'Adventures are great!' );
+}
+else {
+    console.log( 'How about we stay home?' );
+}
 
 // 14 - Create a compound conditional: if luckyNumber is 2 and adventurous is true,
 // console log "Roll the dice!"
+if( luckyNumber = 2 && adventurous ){
+    console.log( 'Roll the Dice!' );
+}
 
-// 15 - Write a conditional that console logs "I can have more pets!" 
+// 15 - Write a conditional that console logs "I can have more pets!"
 //if the value of `pets` is less than the value of `allowedPets`,
 // console logs "I have enough pets" if the value of `pets` is equal to the value of `allowedPets`,
 // and console logs "Oh no, I have too many pets!"
 // if the value of `pets` is greater than the value of `allowedPets`.
+if ( pets < allowedPets ) {
+    console.log( 'I can have more pets!' );
+}
+else if ( pets === allowedPets ) {
+    console.log( 'I have enough pets' );
+}
+else if ( pets > allowedPets ) {
+    console.log( 'Oh no, I have too many pets!' );
+}
 
 // STRETCH GOALS:
 
